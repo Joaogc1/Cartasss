602aecabbfcb6b3e1fe67bdd88112140acb4b9d7
Joaogc1 <joao.coelho.soares@escola.pr.gov.br>
2025-10-29 15:10:48 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vikings',
+    'Quem é pai de Bjorn Ironside?',
+    'Ragnar Lothbrok'
+)
+
+criaCartao(
+    'Dexter',
+    'Dexter Morgan tinha um irmão? Se sim, quem?',
+    'Sim, Brian Moser'
+)
+
+criaCartao(
+    '',
+    'Qual era o grande sonho de Ragnar Lothbrok?',
+    'Explorar e conquistar terras ao oeste.'
+)
+
+criaCartao(
+    '',
+    'Quem é o protagonista de Red Dead Redemption 2?',
+    'Arthur Morgan.'
+)
